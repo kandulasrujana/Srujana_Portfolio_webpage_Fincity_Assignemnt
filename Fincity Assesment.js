65604f7db34f6663f73be69a1c46cc22721bb1ba
kandulasrujana <124784581+kandulasrujana@users.noreply.github.com>
2024-01-25 16:24:31 +0530

    Add files via upload

---
+++ Fincity Assesment.js
@@ -0,0 +1,17 @@
+document.getElementById('form-container').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').slice(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
